f28b0921c063d8becb5310441713385fd8fb2bbd
andrew-lange13 <andrew.lange13@gmail.com>
2022-04-18 15:44:52 -0500

    adds roll dice guess get mapping

--- src/main/java/com/codeup/springblog/RollDiceController.java
+++ src/main/java/com/codeup/springblog/RollDiceController.java
@@ -2,12 +2,18 @@ package com.codeup.springblog;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class RollDiceController {
-    @GetMapping("/roll-dice/{n}")
+    @GetMapping("/roll-dice")
     public String rollDiceForm(){
+        return "roll-dice";
+    }
+
+    @GetMapping("/roll-dice/{n}")
+    public String rollDiceGuess(@PathVariable int n){
         return "roll-dice";
     }
 
