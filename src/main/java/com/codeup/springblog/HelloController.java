a5f35a4006439a3baa359ea64a567acaa4293735
andrew-lange13 <andrew.lange13@gmail.com>
2022-04-18 15:11:19 -0500

    adds join get and post mapping

--- src/main/java/com/codeup/springblog/HelloController.java
+++ src/main/java/com/codeup/springblog/HelloController.java
@@ -23,4 +23,15 @@ public class HelloController {
     public String addOne(@PathVariable int number) {
         return number + " plus one is " + (number + 1) + "!";
     }
+
+    @GetMapping("/join")
+    public String showJoinForm() {
+        return "join";
+    }
+
+    @PostMapping("/join")
+    public String joinCohort(@RequestParam(name = "cohort") String cohort, Model model){
+        model.addAttribute("cohort", "Welcome to " + cohort + "!");
+        return "join";
+    }
 }
