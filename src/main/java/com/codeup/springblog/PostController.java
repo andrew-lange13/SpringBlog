0f2eaaedf80d2f8bbe691444096f699ecaed0711
andrew-lange13 <andrew.lange13@gmail.com>
2022-04-18 14:30:17 -0500

    creates post methods

--- src/main/java/com/codeup/springblog/PostController.java
+++ src/main/java/com/codeup/springblog/PostController.java
@@ -2,6 +2,8 @@ package com.codeup.springblog;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -10,5 +12,20 @@ public class PostController {
     @ResponseBody
     public String posts(){
         return "This is the posts index page";
+    }
+    @GetMapping("/posts/{id}")
+    @ResponseBody
+    public String singlePost(@PathVariable int id){
+        return "This is the number " + id + " post";
+    }
+    @GetMapping("/posts/create")
+    @ResponseBody
+    public String createPostGet(){
+        return "This is the create a post get page";
+    }
+    @PostMapping("/posts/create")
+    @ResponseBody
+    public String createPost(){
+        return "This is the create a post post page";
     }
 }
