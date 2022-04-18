80ff0c4fb474d934b5c66f3a884f3b479d755743
andrew-lange13 <andrew.lange13@gmail.com>
2022-04-18 14:10:15 -0500

    adds addition fucntion

--- src/main/java/com/codeup/springblog/MathController.java
+++ src/main/java/com/codeup/springblog/MathController.java
@@ -1,4 +1,15 @@
 package com.codeup.springblog;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class MathController {
+    @GetMapping("/add/{x}/and/{y}")
+    @ResponseBody
+    public String add(@PathVariable int x, @PathVariable int y){
+        return "Your addition equals " + (x+y);
+    }
 }
