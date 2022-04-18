25607be5eb27e4387b20e8705efc185a888828ae
andrew-lange13 <andrew.lange13@gmail.com>
2022-04-18 13:59:38 -0500

    adds hello methods

--- src/main/java/com/codeup/springblog/HelloController.java
+++ src/main/java/com/codeup/springblog/HelloController.java
@@ -1,14 +1,25 @@
 package com.codeup.springblog;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HelloController {
     @GetMapping("/hello")
     @ResponseBody
-    public String hello(){
-        return "Hello from Spring!";
+    public String hello() {
+        return "<h1>Hello from Spring!</h1>";
+    }
+
+    @GetMapping("/hello/{name}")
+    @ResponseBody
+    public String sayHello(@PathVariable String name) {
+        return "Hello " + name + "!";
+    }
+
+    @RequestMapping(path = "/increment/{number}", method = RequestMethod.GET)
+    @ResponseBody
+    public String addOne(@PathVariable int number) {
+        return number + " plus one is " + (number + 1) + "!";
     }
 }
