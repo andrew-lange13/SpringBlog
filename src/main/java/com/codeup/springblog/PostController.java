1e475f74236ae631c2fc3c1071ae3e5cad93ad67
andrew-lange13 <andrew.lange13@gmail.com>
2022-04-18 14:25:42 -0500

    adds posts mapping

--- src/main/java/com/codeup/springblog/PostController.java
+++ src/main/java/com/codeup/springblog/PostController.java
@@ -1,4 +1,14 @@
 package com.codeup.springblog;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class PostController {
+    @GetMapping("/posts")
+    @ResponseBody
+    public String posts(){
+        return "This is the posts index page";
+    }
 }
