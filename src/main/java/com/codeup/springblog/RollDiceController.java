9e2b9f9e03bee597047e497b9146fee57cc70386
andrew-lange13 <andrew.lange13@gmail.com>
2022-04-18 17:07:45 -0500

    displays guessed number

--- src/main/java/com/codeup/springblog/RollDiceController.java
+++ src/main/java/com/codeup/springblog/RollDiceController.java
@@ -1,6 +1,7 @@
 package com.codeup.springblog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,12 +14,10 @@ public class RollDiceController {
     }
 
     @GetMapping("/roll-dice/{n}")
-    public String rollDiceGuess(@PathVariable int n){
+    public String rollDiceGuess(@PathVariable int n, Model model){
+        model.addAttribute("guessedNumber", n);
         return "roll-dice";
     }
 
-    @PostMapping("/roll-dice")
-    public String rollDiceDisplay(){
-        return "roll-dice";
-    }
+
 }
