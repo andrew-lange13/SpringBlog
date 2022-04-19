21748fb5c79e09187a03dfd621df9b623ce5cd51
andrew-lange13 <andrew.lange13@gmail.com>
2022-04-19 10:54:45 -0500

    passes array list through controller

--- src/main/java/com/codeup/springblog/PostController.java
+++ src/main/java/com/codeup/springblog/PostController.java
@@ -7,11 +7,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+
 @Controller
 public class PostController {
     @GetMapping("/posts")
-    public String posts(){
-
+    public String posts(Model model){
+        ArrayList<Post> postsIndex = new ArrayList<>();
+        postsIndex.add(new Post("first", "i like turtles"));
+        postsIndex.add(new Post("second", "if you ain't first you're last"));
+        model.addAttribute("createdIndex", postsIndex);
         return "posts/index";
     }
     @GetMapping("/posts/{id}")
