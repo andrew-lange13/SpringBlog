e244ff21164efc3c704bfc72be070a761931a66d
andrew-lange13 <andrew.lange13@gmail.com>
2022-04-20 10:10:31 -0500

    defines user table

--- src/main/java/com/codeup/springblog/User.java
+++ src/main/java/com/codeup/springblog/User.java
@@ -1,4 +1,20 @@
 package com.codeup.springblog;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
 }
