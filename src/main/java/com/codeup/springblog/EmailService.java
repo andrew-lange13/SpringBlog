a81a0f246d98903cc169d0ca0eb94d7e68e6a6d6
andrew-lange13 <andrew.lange13@gmail.com>
2022-04-21 11:56:15 -0500

    duplicates mail sending method from curriculum

--- src/main/java/com/codeup/springblog/EmailService.java
+++ src/main/java/com/codeup/springblog/EmailService.java
@@ -2,6 +2,8 @@ package com.codeup.springblog;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
@@ -13,4 +15,18 @@ public class EmailService {
 
     @Value("${spring.mail.from}")
     private String from;
+
+    public void prepareAndSend(Post post, String subject, String body){
+        SimpleMailMessage msg = new SimpleMailMessage();
+        msg.setFrom(from);
+        msg.setTo(post.getOwner().getEmail());
+        msg.setSubject(subject);
+        msg.setText(body);
+
+        try {
+            this.emailSender.send(msg);
+        }catch (MailException e){
+            System.err.println(e.getMessage());
+        }
+    }
 }
