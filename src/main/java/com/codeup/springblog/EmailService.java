52eff94d6260bcc176e9a11293c96fff9bc0a9b7
andrew-lange13 <andrew.lange13@gmail.com>
2022-04-21 11:48:36 -0500

    adds annotations

--- src/main/java/com/codeup/springblog/EmailService.java
+++ src/main/java/com/codeup/springblog/EmailService.java
@@ -1,4 +1,16 @@
 package com.codeup.springblog;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service("mailService")
 public class EmailService {
+
+    @Autowired
+    public JavaMailSender emailSender;
+
+    @Value("${spring.mail.from}")
+    private String from;
 }
