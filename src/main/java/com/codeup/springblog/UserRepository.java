91ec5a75cbac3507b4a130f51409a1427267edae
andrew-lange13 <andrew.lange13@gmail.com>
2022-04-21 14:04:24 -0500

    adds find by username method

--- src/main/java/com/codeup/springblog/UserRepository.java
+++ src/main/java/com/codeup/springblog/UserRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User getUserById(long id);
+    User findByUsername(String username);
 }
