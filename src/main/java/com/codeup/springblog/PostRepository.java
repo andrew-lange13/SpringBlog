8fed25a414fce077d312ad1bf90e8d8b9700cf8e
andrew-lange13 <andrew.lange13@gmail.com>
2022-04-21 10:09:47 -0500

    adds get post by id method

--- src/main/java/com/codeup/springblog/PostRepository.java
+++ src/main/java/com/codeup/springblog/PostRepository.java
@@ -3,6 +3,5 @@ package com.codeup.springblog;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-    Post getById(long id);
-
+    Post getPostById(long id);
 }
