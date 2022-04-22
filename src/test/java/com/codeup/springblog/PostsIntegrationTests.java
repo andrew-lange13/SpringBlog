a7929e0b3d08771b0d300bcf9fcf7accabdc3ca9
andrew-lange13 <andrew.lange13@gmail.com>
2022-04-22 09:37:30 -0500

    adds sanity checks

--- src/test/java/com/codeup/springblog/PostsIntegrationTests.java
+++ src/test/java/com/codeup/springblog/PostsIntegrationTests.java
@@ -4,6 +4,7 @@ import com.codeup.springblog.models.User;
 import com.codeup.springblog.repositories.PostRepository;
 import com.codeup.springblog.repositories.UserRepository;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -15,6 +16,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import javax.servlet.http.HttpSession;
 
+import static org.junit.Assert.assertNotNull;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
@@ -62,5 +64,15 @@ public class PostsIntegrationTests {
                 .getRequest()
                 .getSession();
 
+    }
+
+    @Test
+    public void contextLoads(){
+        assertNotNull(mvc);
+    }
+
+    @Test
+    public void testIfUserSessionIsActive() throws Exception{
+        assertNotNull(httpSession);
     }
 }
